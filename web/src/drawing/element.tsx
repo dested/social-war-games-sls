1d06db37444e4a2e792f610e9d4a7ce5ce37d4c9
Sal Aiello <sala@axonet.local>
2018-05-29 17:59:13 -0700

    more jsx testing

--- web/src/drawing/element.tsx
+++ web/src/drawing/element.tsx
@@ -1,10 +1,13 @@
-/** @jsx dom */
-import { dom } from "./renderer"
+/** @jsx dCanvas */
+import { dCanvas } from "./dCanvas"
+import { render } from "./renderer"
 
 function foo() {
+  const m = [1, 2, 3, 4];
   return <canvas foo={12}>
     <image foo={17}></image>
-    <image foo={17}></image>
+    {m.map(a => <image foo={a}></image>)}
+    <text>f</text>
     <image foo={17}></image>
     <Foo foo={17}>
       <image foo={17}></image>
@@ -16,3 +19,5 @@ function foo() {
 function Foo(options: { foo: number }, children: any) {
   return <image foo={12}>{children}</image>
 }
+
+render(foo());
---
+++ web/src/drawing/renderer.ts
@@ -0,0 +1,11 @@
+import { dCanvas } from "./dCanvas";
+
+const canvases={};
+export function render(element: dCanvas.JSX.Element) {
+  switch(element.elementName){
+    case 'canvas':
+
+    break;
+  }
+  
+}
